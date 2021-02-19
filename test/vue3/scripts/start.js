678ae1f85f8f82900f2022ac686c0fa33dc786ca
ambit <ambit_tsai@qq.com>
2021-02-19 21:04:05 +0800

    test: reverse proxy

--- test/vue3/scripts/start.js
+++ test/vue3/scripts/start.js
@@ -7,5 +7,15 @@ module.exports = {
     server: {
         port: 8000,
         open: true,
+        proxy: {
+            '/react/': {
+                target: 'http://127.0.0.1:8001/',
+                rewrite: path => path.replace(/^\/react\//, '/'),
+            },
+            '/vue2/': {
+                target: 'http://127.0.0.1:8002/',
+                rewrite: path => path.replace(/^\/vue2\//, '/'),
+            },
+        },
     },
 }
