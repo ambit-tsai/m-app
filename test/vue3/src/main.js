decdc0aad26a3d6b8b539d30ee097b14a25b1507
ambit <ambit_tsai@qq.com>
2021-02-21 20:10:36 +0800

    test<vue3>: redirect 404 to the correct path

--- test/vue3/src/main.js
+++ test/vue3/src/main.js
@@ -2,14 +2,8 @@ import { createApp, h } from 'vue';
 import 'element-plus/lib/theme-chalk/index.css';
 import App from './App.vue';
 import router from './router';
+import { PROJECT_NAME } from './constant';
 import '@ambit_tsai/m-app';
-
-
-const redirect = localStorage.getItem('redirect');
-if (redirect) {
-    localStorage.setItem('redirect', '');
-    router.replace(redirect);
-}
 
 
 const app = createApp({
@@ -19,3 +13,11 @@ app.config.devtools = true;
 app.config.isCustomElement = tag => tag.startsWith('m-');
 app.use(router);
 app.mount(document.body);
+
+
+// 404 redirect
+const redirect = localStorage.getItem('redirect');
+if (redirect) {
+    localStorage.setItem('redirect', '');
+    router.replace(redirect.replace(PROJECT_NAME, '/'));
+}
