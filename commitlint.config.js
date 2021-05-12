f9f635fb6d32053a763bf9dba9d2411ad1c7ea06
fanwei <fanwei@gaoding.com>
2021-05-12 12:03:38 +0800

    chore: add commitlint

---
+++ commitlint.config.js
@@ -0,0 +1,19 @@
+/**
+ * Available types:
+ *   build
+ *   ci
+ *   chore
+ *   docs
+ *   feat
+ *   fix
+ *   perf
+ *   refactor
+ *   revert
+ *   style
+ *   test
+ */
+module.exports = {
+    extends: [
+        '@commitlint/config-conventional'
+    ],
+}
